717798b5de6df0a4b2d2f56ce83b3c9f0ca69532
Anton Kristiono <antonkristiono47@gmail.com>
2021-03-05 20:38:14 +0700

    add tailwind and  start header

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,47 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  // <header
+  //   style={{
+  //     background: `rebeccapurple`,
+  //     marginBottom: `1.45rem`,
+  //   }}
+  // >
+  //   <div
+  //     style={{
+  //       margin: `0 auto`,
+  //       maxWidth: 960,
+  //       padding: `1.45rem 1.0875rem`,
+  //     }}
+  //   >
+  //     <h1 style={{ margin: 0 }}>
+  //       <Link
+  //         to="/"
+  //         style={{
+  //           color: `white`,
+  //           textDecoration: `none`,
+  //         }}
+  //       >
+  //         {siteTitle}
+  //       </Link>
+  //     </h1>
+  //   </div>
+  // </header>
+  <div>
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <Link to="/">
+                <h2 className="text-white">{siteTitle}</h2>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
 )
 
 Header.propTypes = {
